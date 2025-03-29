3e4fe54dd2f3302b21ef779949c6619fad0c12b3
Voyage <kirillbatalovwork@mail.ru>
2025-03-29 17:52:20 +0500

    staticfiles

--- trading_platform/trading_platform/settings.py
+++ trading_platform/trading_platform/settings.py
@@ -118,6 +118,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'frontend/static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
